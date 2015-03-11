0bcab089a011808c6bb283b7c1732c37d7afb5f9
MrMoDDoM <danielebarattieri@gmail.com>
2015-03-11 12:14:37 +0100

    Crata inizializzazione tabellone

--- Monopoli/src/main/main.java
+++ Monopoli/src/main/main.java
@@ -11,15 +11,22 @@ public class main {
 	private MyMenu menu;
 	
 	public main(){
+		inizializzazione();
+		
+		//Fase di creazione giocatori
 		
 	}
 	
+	
+	//Crea e instanzia tutte le caselle del tabellone
 	private void inizializzazione(){
 		tabellone = new Tabellone();
 		
+		String [] nomi_caselle = Data.getNomiCaselle();
+		
 		int i;
 		for(i=0; i<40; i++){
-			
+			tabellone.aggiungiCasella(new Casella(nomi_caselle[i], i));
 		}
 	}
 }
