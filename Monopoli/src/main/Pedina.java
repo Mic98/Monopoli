e526f971723969fdf763ab262a4a099ccd482a9b
CarloG91 <c.giannini91@gmail.com>
2015-03-11 11:42:38 +0100

    Inserimento classe pedina

---
+++ Monopoli/src/main/Pedina.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Carlo
+ *
+ */
+public class Pedina {
+	
+	private int posizione;
+	
+	
+	public Pedina(int posizione){
+		
+		this.posizione=posizione;
+		
+	}
+
+
+	public int getPosizione() {
+		return posizione;
+	}
+
+
+	public void setPosizione(int posizione) {
+		this.posizione = posizione;
+	}
+
+	
+	
+}
