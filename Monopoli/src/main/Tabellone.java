979e07af6c8d4c5efa6b8f8d70d220f911c1db7b
CarloG91 <c.giannini91@gmail.com>
2015-05-19 10:32:08 +0200

    reinserimento classi

---
+++ Monopoli/src/main/Tabellone.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package main;
+
+import java.util.Vector;
+
+/**
+ * @author mrmoddom
+ *
+ */
+public class Tabellone {
+
+	private Vector<Casella> caselle;
+	
+	public Tabellone(){
+		caselle = new Vector<Casella>();
+	}
+	
+	public Casella aggiungiCasella(Casella in){
+		caselle.add(in);
+		return in;
+	}
+}
